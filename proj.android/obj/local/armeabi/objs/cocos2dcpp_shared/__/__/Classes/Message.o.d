c0bc4118885419595ff460b804298636df3902b4
wangxuanyi <wangxuanyihaha@gmail.com>
2014-05-12 17:47:46 +0800

    delete obj

--- proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/Message.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/Message.o: \
- jni/../../Classes/Message.cpp jni/../../Classes/Message.h
-
-jni/../../Classes/Message.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /usr/local/Cellar/cocos2d-x-2.2.3/projects/Four/proj.android/../../../cocos2dx/ccFPSImages.c \
- /usr/local/Cellar/cocos2d-x-2.2.3/projects/Four/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/usr/local/Cellar/cocos2d-x-2.2.3/projects/Four/proj.android/../../../cocos2dx/ccFPSImages.h:
