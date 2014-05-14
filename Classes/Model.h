0e6caf42e3d09b6cd7e027e5f56cb51bc5283498
wangxuanyi <wangxuanyihaha@gmail.com>
2014-05-14 20:14:35 +0800

    补充了一些注释

--- Classes/Model.h
+++ Classes/Model.h
@@ -37,18 +37,24 @@ public:
     
     virtual ~Model(){}
     
-    Model* addTransition(const string& from, const string& event, const string& to);
+    // 添加一条状态转换，from-起始状态，msg-接收的消息，to-终结状态，在msg发生时会发生状态转换
+    Model* addTransition(const string& from, const string& msg, const string& to);
     
+    // 检查当前状态机能否发生msg对应的状态转换
     bool checkMessage(const string& msg);
     
+    // 触发msg对应的状态转换
     void onMessage(const string& msg);
     void onMessage(const string& msg, CCObject* o);
     
-    void onAsyncMessage(CCNode* node , CCString* msg);
-    
+    // 等待某个CCAction结束后发送一条消息。
     void waitAction(cocos2d::CCNode* node, cocos2d::CCFiniteTimeAction* action, const string& msg);
     
+    // 得到当前状态名称
     const char* getState();
+    
+    // callback
+    void onAsyncMessage(CCNode* node , CCString* msg);
 };
 
 #endif /* defined(__Four__Model__) */
